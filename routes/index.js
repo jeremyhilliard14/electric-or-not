c8c74909bd16090a6964206b9ae2267eb5d0a0ed
jeremyhilliard14 <jeremyhilliard14@gmail.com>
2016-04-25 10:22:09 -0400

    added compass and additional files

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb');
+var mongoClient = mongo.MongoClient;
+var mongoUrl = 'mongodb://localhost:27017/electricOrNot';
+
+var db;
+
+mongoClient.connect(mongoUrl, function(error, database){
+	db = database;
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	// var cars = db.collection('cars').insert({name: 'Buick'});
+	db.collection('cars').find({}).toArray(function(error, carResult){
+		console.log(carResult);
+		// for(i=0; i<carResult.length; i++){
+		// 	console.log(carResult[i].imageSrc);
+		// }
+		var getRandomImage = Math.floor(Math.random() * carResult.length);
+		//console.log(getRandomImage);
+		res.render('index', { carImage: carResult[getRandomImage] });
+	});
+	//console.log(cars);
+  // res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
