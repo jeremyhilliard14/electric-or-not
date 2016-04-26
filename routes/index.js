9d30a5e75d40eef824ab3c2fa408b4d536d4040d
jeremyhilliard14 <jeremyhilliard14@gmail.com>
2016-04-26 10:13:40 -0400

    updated sass file name

--- routes/index.js
+++ routes/index.js
@@ -5,19 +5,37 @@ var mongoClient = mongo.MongoClient;
 var mongoUrl = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost:27017/electricOrNot';
 
 var db;
+var allPhotos;
 
 mongoClient.connect(mongoUrl, function(error, database){
-	db = database;
+	database.collection('cars').find().toArray(function(error, result){
+		allPhotos = result;
+		db = database;
+		console.log(allPhotos);
+	});
+	
 });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('index', {title: "Express"});
+	// res.render('index', {title: "Express"});
 	// 1. Get all pics from MongoClient
+	// this takes place when we connect
 	// 2. Get the current user from mongo
+	var currIP = req.ip;
+	console.log("The current user's IP address is " + currIP);
+	db.collection('users').find({ip: currIP}).toArray(function(error, userResult){
+		//if the user result returns nothing, then the user hasn't voted on anything.
+			// 4. load all docs into an array
+		if(userResult.length == 0){
+		photosToShow = allPhotos;
+		}
+		// 5. pick a random one
+		var getRandomImage = Math.floor(Math.random() * photosToShow.length);
+		res.render('index', { carImage: allPhotos[getRandomImage] });
+
+	});
 	// 3. find out which pics the user has voted on.
-	// 4. load all docs into an array
-	// 5. pick a random one
 	// 6. send the random pic to the view.
 	// var cars = db.collection('cars').insert({name: 'Buick'});
 	// db.collection('cars').find({}).toArray(function(error, carResult){
@@ -31,26 +49,44 @@ router.get('/', function(req, res, next) {
 	// });
 	//console.log(cars);
   // res.render('index', { title: 'Express' });
+  
 });
 
 router.post('/electric', function(req, res, next){
 	//res.send(req.body);
+
+	db.collection('cars').updateOne(
+		{imageSrc: req.body.photo},
+		{
+			$set: {"totalVotes": 1}
+		}, function(error, results){
+			console.log(results);
+		}
+	)
+	
+
+
+
 	res.send("The user chose " + req.body.photo + " as an electric vehicle.")
+
 	// 1. We know they voted electric or they wouldn't be here.
 	// 2. we know what they voted on bc we passed it in the req.body var
 	// 3. we know who they are bc of their IP
 	// 4.  update user table to include IP and pic they voted on.
 	// 5. update the images/cars collection by one
+	// 6. send them back to the main page so they can vote again
+	// 6b. if the user has voted on every image in the database, notify them.
 });
 
-router.post('/poser', function(req, res, next){
+router.post('/notElectric', function(req, res, next){
 	//res.send(req.body);
-	res.send("The user chose " + req.body.photo + " as a poser vehicle.")
+	res.send("The user chose " + req.body.photo + " as a not an electric vehicle.")
 	// 1. We know they voted electric or they wouldn't be here.
 	// 2. we know what they voted on bc we passed it in the req.body var
 	// 3. we know who they are bc of their IP
 	// 4.  update user table to include IP and pic they voted on.
 	// 5. update the images/cars collection by -1
+	// 6. send them back to the main page so they can vote again
 });
 
 module.exports = router;
